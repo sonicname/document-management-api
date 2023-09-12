a1351ca3c6bf0f207666e066dc9fdf7dd394f745
phamduck <phamanhduc2k2@gmail.com>
2023-09-12 09:48:00 +0700

    change to boilerplate

--- dockerfile
+++ dockerfile
@@ -1,23 +1,20 @@
-FROM node:18-alpine
+FROM node:18.17.0-alpine
 
-ARG NODE_ENV=production
-ENV NODE_ENV=${NODE_ENV}
+RUN apk add --no-cache bash
+RUN npm i -g @nestjs/cli typescript ts-node
 
-RUN mkdir -p /usr/src/app
-
-WORKDIR /usr/src/app
-
-COPY package.json /usr/src/app
-
-RUN npm install -g @nestjs/cli
-
-RUN npm install
+COPY package*.json /tmp/app/
+RUN cd /tmp/app && npm install
 
 COPY . /usr/src/app
+RUN cp -a /tmp/app/node_modules /usr/src/app
+COPY ./wait-for-it.sh /opt/wait-for-it.sh
+COPY ./startup.dev.sh /opt/startup.dev.sh
+RUN sed -i 's/\r//g' /opt/wait-for-it.sh
+RUN sed -i 's/\r//g' /opt/startup.dev.sh
 
+WORKDIR /usr/src/app
+RUN if [ ! -f .env ]; then cp env-example .env; fi
 RUN npm run build
 
-RUN cd /usr/src/app/
-
-CMD [ "node", "dist/main" ]
-
+CMD ["/opt/startup.dev.sh"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,92 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HeaderResolver } from 'nestjs-i18n';
+import { I18nModule } from 'nestjs-i18n/dist/i18n.module';
+import path from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { AuthAppleModule } from './auth-apple/auth-apple.module';
+import { AuthFacebookModule } from './auth-facebook/auth-facebook.module';
+import { AuthGoogleModule } from './auth-google/auth-google.module';
+import { AuthTwitterModule } from './auth-twitter/auth-twitter.module';
+import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
+import appleConfig from './config/apple.config';
+import authConfig from './config/auth.config';
+import { AllConfigType } from './config/config.type';
+import databaseConfig from './config/database.config';
+import facebookConfig from './config/facebook.config';
+import fileConfig from './config/file.config';
+import googleConfig from './config/google.config';
+import mailConfig from './config/mail.config';
+import twitterConfig from './config/twitter.config';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
+import { FilesModule } from './files/files.module';
+import { ForgotModule } from './forgot/forgot.module';
+import { HomeModule } from './home/home.module';
+import { MailModule } from './mail/mail.module';
+import { MailerModule } from './mailer/mailer.module';
+import { SessionModule } from './session/session.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        authConfig,
+        appConfig,
+        mailConfig,
+        fileConfig,
+        facebookConfig,
+        googleConfig,
+        twitterConfig,
+        appleConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+      dataSourceFactory: async (options: DataSourceOptions) => {
+        return await new DataSource(options).initialize();
+      },
+    }),
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService<AllConfigType>) => ({
+        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
+          infer: true,
+        }),
+        loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
+      }),
+      resolvers: [
+        {
+          use: HeaderResolver,
+          useFactory: (configService: ConfigService<AllConfigType>) => {
+            return [
+              configService.get('app.headerLanguage', {
+                infer: true,
+              }),
+            ];
+          },
+          inject: [ConfigService],
+        },
+      ],
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+    AuthFacebookModule,
+    AuthGoogleModule,
+    AuthTwitterModule,
+    AuthAppleModule,
+    ForgotModule,
+    SessionModule,
+    MailModule,
+    MailerModule,
+    HomeModule,
+  ],
 })
 export class AppModule {}
